d8dcdf2c46436777d211f9663ab1b0afdcec0e7d
Steven P <steven.stevenparker2@gmail.com>
2026-02-22 15:54:22 -0500

    Delete AssetUploader directory

--- AssetUploader/Http/Routes/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// Admin routes for AssetUploader
-Route::get('/', 'AssetUploaderController@index')->name('index');
-Route::post('/upload', 'AssetUploaderController@store')->name('store');
